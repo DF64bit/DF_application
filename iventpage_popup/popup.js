c267fc97e5107e8c609f43c3ebbb5d8dcd08dd25
DF64bit <tjis4943@gmail.com>
2023-01-11 14:32:08 +0900

    フォルダを作成し、フォルダごとにアプリのhtmlを分けた。

--- iventpage_popup/popup.js
+++ iventpage_popup/popup.js
@@ -1,16 +1,16 @@
 var questionnaire = document.querySelector("#questionnaire");
 questionnaire.addEventListener("click",
 (e)=>{
-    window.open("/../application_flame/questionnaire.html", "window_name", "width=300,height=200,scrollbars=yes");
+    window.open("/../application_flame/questionnaire/questionnaire.html", "window_name", "width=300,height=200,scrollbars=yes");
 
 });
 var wordpad = document.querySelector("#wordpad");
 wordpad.addEventListener("click",
 (e)=>{
-    window.open("/../application_flame/wordpad.html", "window_name", "width=300,height=200,scrollbars=yes");
+    window.open("/../application_flame/wordpad/wordpad.html", "window_name", "width=300,height=200,scrollbars=yes");
 });
 var presentation = document.querySelector("#presentation");
 presentation.addEventListener("click",
 (e)=>{
-    window.open("/../application_flame/presentation.html", "window_name", "width=300,height=200,scrollbars=yes");
+    window.open("/../application_flame/presentation/presentation.html", "window_name", "width=300,height=200,scrollbars=yes");
 })
