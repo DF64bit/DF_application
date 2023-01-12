b6143ea688a06dc32256be30a4f458d971892b49
DF64bit <96969533+DF64bit@users.noreply.github.com>
2023-01-12 13:53:04 +0900

    Update popup.js

--- iventpage_popup/popup.js
+++ iventpage_popup/popup.js
@@ -1,16 +1,23 @@
 var questionnaire = document.querySelector("#questionnaire");
 questionnaire.addEventListener("click",
 (e)=>{
-    window.open("/../application_flame/questionnaire/questionnaire.html", "window_name", "width=300,height=200,scrollbars=yes");
+    window.open("/../application_flame/questionnaire/questionnaire.html", "window_name", "scrollbars=yes");
 
 });
 var wordpad = document.querySelector("#wordpad");
 wordpad.addEventListener("click",
 (e)=>{
-    window.open("/../application_flame/wordpad/wordpad.html", "window_name", "width=300,height=200,scrollbars=yes");
+    window.open("/../application_flame/wordpad/wordpad.html", "window_name", "scrollbars=yes");
 });
 var presentation = document.querySelector("#presentation");
 presentation.addEventListener("click",
 (e)=>{
-    window.open("/../application_flame/presentation/presentation.html", "window_name", "width=300,height=200,scrollbars=yes");
+    window.open("/../application_flame/presentation/presentation.html", "window_name", "scrollbars=yes");
 })
+//   __________　　　____________
+//  |           ＼  |
+//  |             | |
+//  |             | |___________
+//  |             | |
+//  |             | |
+//  |__________ ／  |
