39812899ca3fd8cce3dd3860e93372e28790d34e
DF64bit <96969533+DF64bit@users.noreply.github.com>
2023-01-27 09:57:33 +0900

    ワードパッドの試作品完成したああぁsjlkあdsklf

--- application_flame/wordpad/wordpad.js
+++ application_flame/wordpad/wordpad.js
@@ -1,18 +1,28 @@
 var uploadButton = document.getElementById("upload");
 uploadButton.addEventListener("click",
-(e)=>{
+async (e)=>{
     var dirHandle = await window.showDirectoryPicker();
 
-    const file = await dirHandle.getFileHandle(window.prompt("編集したいファイルの名前をファイル拡張子名も含めて入力してください(txtファイルしか読み込めません)"));
+    const file = await dirHandle.getFileHandle(window.prompt("編集したいファイルの名前をファイル拡張子名も含めて入力してください※txtファイルしか読み込めません"));
 
-    const sampleConfig = window.prompt("このファイルの内容を何に変更しますか?");
+    fileData = await file.getFile();
+    var text = await fileData.text();
+    document.getElementById("other").innerHTML = '<textarea id="content" name="content" rows="5" cols="33">' + text + '</textarea> <button id="save">保存</button>';
+    document.getElementById("inuploadbutton").innerHTML = "";
 
-    const blob = new Blob([sampleConfig], {
-      type: "text/plain"
+    var saveButton = document.getElementById("save");
+    saveButton.addEventListener("click",
+    async (e)=>{
+      const sampleConfig = document.getElementById("content").value;
+
+      const blob = new Blob([sampleConfig], {
+        type: "text/plain"
+      });
+
+      const writableStream = await file.createWritable();
+      await writableStream.write(blob);
+      await writableStream.close();
+      window.alert("保存しました。 ");
+      window.close();
     });
-
-    const writableStream = await file.createWritable();
-    await writableStream.write(blob);
-    await writableStream.close();
-    window.alert("保存しました。 ");
-})
+});
